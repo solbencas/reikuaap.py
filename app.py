d1287014faf628e4715199b75b1d114dbb1c43f3
clauavila <clauavila.sch@gmail.com>
2019-02-11 12:46:14 -0300

    Se agrega base de datos

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
+import sys
+
+DATABASE = 'reikuappdb.db'
+
 app = Flask(__name__)
+
+def get_db():
+   db = getattr(g, '_database', None)
+   if db is None:
+       db = g._database = sqlite3.connect(DATABASE)
+   return db
+
+def query_db(query, args=(), one=False):
+   cur = get_db().execute(query, args)
+   rv = cur.fetchall()
+   cur.close()
+   return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+   lugares = query_db('select * from lugares')
+   print(lugares[0], file=sys.stderr)
+
+
+   return "Hello World"
